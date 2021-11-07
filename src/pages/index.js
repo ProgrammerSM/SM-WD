84b7ae1550d898ab619425f471817f493bb5428b
PSM-Desktop <purelyfreshwateraquariums@gmail.com>
2021-11-07 01:38:38 -0600

    added layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,30 +2,17 @@
 import { Helmet } from 'react-helmet'
 import React from "react"
 
-// Styles
-import "styles/reset.scss"
-import "styles/typography.scss"
+// Components
+import Layout from 'components/global-components/Layout'
 
 // markup
 const IndexPage = () => {
   return (
-    <>
+    <Layout>
       <Helmet>
         <title>Coming Soon</title>
       </Helmet>
-      <main
-        style={{
-          alignItems: 'center',
-          display: 'flex',
-          height: '100vh',
-          justifyContent: 'center',
-          textAlign: 'center', 
-          width: '100vw',
-        }} 
-      >
-        <h1>Website Coming Soon</h1>
-      </main>
-    </>
+    </Layout>
   )
 }
 
