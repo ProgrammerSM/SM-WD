1f041aa54d5fbc353e4867f80d0417a558ca0314
PSM-Desktop <purelyfreshwateraquariums@gmail.com>
2021-11-07 21:05:06 -0700

    added eslint and other packages, and applied initial preferred rules that will be evaluted as I work

--- src/scripts/media-queries.js
+++ src/scripts/media-queries.js
@@ -1,6 +1,6 @@
 export default {
   large: '@media screen and (min-width: 1025px) and (max-width: 1440px)',
-  largUp: '@media screen and (min-width: 1025px)',
+  largeUp: '@media screen and (min-width: 1025px)',
   medium: '@media screen and (min-width: 641px) and (max-width: 1024px)',
   mediumDown: '@media screen and (max-width: 1024px)',
   mediumUp: '@media screen and (min-width: 641px)',
--- src/scripts/themeColorGroups.js
+++ src/scripts/themeColorGroups.js
@@ -1,22 +1,22 @@
 export default {
   dark: {
     colors: {
-      backgroundColor: '#000000',
-      fontColor: '#FFFFFF',
-      primaryColor: '#FFFFFF',
       accentColor1: '#FFFFFF',
       accentColor2: '#FFFFFF',
       accentColor3: '#FFFFFF',
-    }
+      backgroundColor: '#000000',
+      fontColor: '#FFFFFF',
+      primaryColor: '#FFFFFF',
+    },
   },
   light: {
     colors: {
-      backgroundColor: '#FFFFFF',
-      fontColor: '#000000',
-      primaryColor: '#000000',
       accentColor1: '#000000',
       accentColor2: '#000000',
       accentColor3: '#000000',
-    }
-  }
+      backgroundColor: '#FFFFFF',
+      fontColor: '#000000',
+      primaryColor: '#000000',
+    },
+  },
 }
